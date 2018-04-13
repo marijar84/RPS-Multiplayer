ad9f6fb58764ea56bd80c0bcd95cecc75aad143d
Maribel <maribel.jaramillo@vm2020.com>
2018-04-12 17:01:03 -0700

    Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,20 +11,59 @@ firebase.initializeApp(config);
 
 var dataRef = firebase.database();
 
+var imgSelected = "";
+var nameImgSelected = "";
+var nameInput = "";
+
+var userId = 0;
+var countUser = 0;
+
+var player1 = false, player2 = false;
+var opponentOption = "";
+var opponentName = "";
+
+var countWin = 0;
+var countLoose = 0;
+
+window.onbeforeunload = function (evt) {
+    if (typeof evt == 'undefined') {
+        evt = window.event;
+    }
+    if (evt) {
+        dataRef.ref("players/" + userId).remove();
+
+        dataRef.ref("chat").push({
+            userName: "",
+            message: nameInput + " has disconnected!"
+        });
+    }
+}
+
+$(window).on('load', function () {
+    $('#divModal').modal({ backdrop: 'static', keyboard: false })
+    $('#divModal').modal('show');
+});
+
 $(document).on("click", "#saveUser", function (event) {
     event.preventDefault();
 
-    console.log("save user");
+    $('#divModal').modal('hide');
 
-    var nameInput = $("#userName").val().trim();
+    nameInput = $("#userName").val().trim();
 
-    $("#user").text(nameInput);
-    console.log(nameInput);
+    $("#player1").text("Hi! " + nameInput);
 
-    dataRef.ref("user").push({
+    dataRef.ref("chat").remove();
+    $("chats").empty();
+
+
+    userId = countUser;
+    dataRef.ref("players/" + countUser).set({
         name: nameInput,
+        option: "",
         losses: 0,
-        win: 0
+        win: 0,
+        played: false
     });
 });
 
@@ -33,9 +72,6 @@ $(document).on("click", "#sendMessage", function (event) {
 
     var sendMessage = $("#textArea").val().trim();
     var user = $("#user").text();
-
-    console.log(user);
-    console.log(sendMessage);
 
     dataRef.ref("chat").push({
         userName: user,
@@ -46,19 +82,160 @@ $(document).on("click", "#sendMessage", function (event) {
 });
 
 $(document).on("click", ".image", function (event) {
-    console.log(event);
-    console.log(event.originalEvent.target.alt);
+    event.preventDefault();
+
+    var imgActual = event.originalEvent.target.alt;
+
+    if (nameImgSelected === "") {
+        imgSelected = $(this);
+        nameImgSelected = event.originalEvent.target.alt;
+
+        imgSelected.addClass("selectImage");
+    }
+    else {
+        imgSelected.removeClass("selectImage");
+
+        imgSelected = $(this);
+        nameImgSelected = event.originalEvent.target.alt;
+
+        imgSelected.addClass("selectImage");
+    }
+
+    $("#waiting").text("Waiting to " + opponentName);
+
+    dataRef.ref("players/" + userId).set({
+        name: nameInput,
+        option: imgActual,
+        losses: 0,
+        win: 0,
+        played: true
+    });
 });
 
+dataRef.ref("chat").on("child_added", function (childSnapshot) {
 
-dataRef.ref("chat").on("child_added", function(childSnapshot) {
-    console.log(childSnapshot);
     var option = $("<option>");
 
-    var htmlText = "<h2>" + childSnapshot.val().userName +"  says: </h2><p>" + childSnapshot.val().message+"</p>"
+    var htmlText = "";
 
+    console.log("chat",  childSnapshot.val().userName);
+
+    if (childSnapshot.val().message.includes("disconnected") == true) {
+        htmlText = "<var>" + childSnapshot.val().message + "</var>"
+    }
+    else {
+        htmlText = "<strong>" + childSnapshot.val().userName + "  says: </strong><p>" + childSnapshot.val().message + "</p>"
+    }
     option.html(htmlText);
 
     $("#chats").append(option);
 });
 
+/*dataRef.ref("chat").on("child_removed", function (childSnapshot) {
+    console.log("Remover chat");
+    dataRef.ref("chat").remove();
+});*/
+
+
+dataRef.ref("players").on("child_changed", function (childSnapshot) {
+    if (childSnapshot.val().name != nameInput) {
+        opponentOption = childSnapshot.val().option;
+
+        player2 = childSnapshot.val().played;
+
+        $("#winOpponent").text(childSnapshot.val().win);
+        $("#looseOpponent").text(childSnapshot.val().losses);
+    }
+    else {
+        player1 = childSnapshot.val().played;
+
+        $("#winMyCount").text(childSnapshot.val().win);
+        $("#looseMyCount").text(childSnapshot.val().losses);
+    }
+
+    if (player1 === true && player2 === true) {
+        setOpponentImage(opponentOption);
+
+        validateOption(nameImgSelected, opponentOption);
+    }
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
+
+dataRef.ref("players").on("child_added", function (childSnapshot) {
+    var data = childSnapshot.val();
+    opponentName = childSnapshot.val().name;
+    countUser++;
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
+
+dataRef.ref("players").on("value", function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
+        //console.log("value", childSnapshot.val().name);
+    });
+}, function (errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+});
+
+function setOpponentImage(opponentOption) {
+    $("#opponent").css("visibility", "visible");
+    $("#waiting").text("");
+    if (opponentOption === "Scissors") {
+        $("#opponent").attr("src", "assets/images/scissor.jpg");
+    }
+    else if (opponentOption === "Paper") {
+        $("#opponent").attr("src", "assets/images/paper.jpg");
+    }
+    else if (opponentOption === "Rock") {
+        $("#opponent").attr("src", "assets/images/rock.png");
+    }
+}
+
+function validateOption(myOption, opponentOption) {
+    if (myOption === "Rock" && opponentOption === "Scissors") {
+        countWin++;
+        $("#titleResult").text("You win!");
+    }
+    else if (myOption === "Rock" && opponentOption === "Paper") {
+        countLoose++;
+        $("#titleResult").text("You loose!");
+    }
+    else if (myOption === "Scissors" && opponentOption === "Rock") {
+        countLoose++;
+        $("#titleResult").text("You loose!");
+    }
+    else if (myOption === "Scissors" && opponentOption === "Paper") {
+        countWin++;
+        $("#titleResult").text("You win!");
+    }
+    else if (myOption === "Paper" && opponentOption === "Rock") {
+        countWin++;
+        $("#titleResult").text("You win!");
+    }
+    else if (myOption === "Paper" && opponentOption === "Rock") {
+        countLoose++;
+        $("#titleResult").text("You loose!");
+    }
+    else if (myOption === opponentOption) {
+        $("#titleResult").text("TIE!");
+    }
+    var timer = setTimeout(myTimer, 5000);
+    clearTimeout();
+}
+
+function myTimer() {
+    $("#titleResult").text("");
+    $("#opponent").css("visibility", "hidden");
+    imgSelected.removeClass("selectImage");
+
+    dataRef.ref("players/" + userId).set({
+        name: nameInput,
+        option: "",
+        losses: countLoose,
+        win: countWin,
+        played: false
+    });
+
+
+}
